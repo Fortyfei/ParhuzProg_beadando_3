017c00f325cf6ce1c1657ff26cfc84223a7da8d3
Fortyfei <54122927+Fortyfei@users.noreply.github.com>
2020-05-01 14:06:33 +0200

    Update beadando_3.c

--- beadando_3/beadando_3.c
+++ beadando_3/beadando_3.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <math.h>
+#include <omp.h>
 
 #define PI 3.1415926535897932
 #define N 16 //size of the plate
 
 #define SIZE N+2
-#define ITERATIONS 100*100*100*10
+#define ITERATIONS 100000000
 
 void printMatrix(int A[SIZE][SIZE]) {
     for (int i = 0; i < SIZE; ++i) {
@@ -16,27 +17,82 @@ void printMatrix(int A[SIZE][SIZE]) {
     }
 }
 
-void sequential(int A[SIZE][SIZE]) {
-    printf("\n\nSEQUENTIAL\n");
-    for (int it = 0; it < ITERATIONS; ++it) {
-        for (int i = 1; i < SIZE - 1; ++i) {
-            for (int j = 1; j < SIZE - 1; ++j) {
-                int n1 = A[i - 1][j];
-                int n2 = A[i + 1][j];
-                int n3 = A[i][j - 1];
-                int n4 = A[i][j + 1];
-                double num = (n1 + n2 + n3 + n4) / 4;
-                A[i][j] = round(num);
+void printTime(double s, double e) {
+    printf("Elapsed time: %f s\n", e - s);
+}
+
+void sequentialJacobi(int A[SIZE][SIZE]) {
+
+    printf("\n\nSEQUENTIAL W/JACOBI\n");
+    //takes the avarage of the neighbouring points
+    double start = omp_get_wtime();
+    {
+        for (int it = 0; it < ITERATIONS; ++it) {
+            for (int i = 1; i < SIZE - 1; ++i) {
+                for (int j = 1; j < SIZE - 1; ++j) {
+                    int n1 = A[i - 1][j];
+                    int n2 = A[i + 1][j];
+                    int n3 = A[i][j - 1];
+                    int n4 = A[i][j + 1];
+                    double num = (n1 + n2 + n3 + n4) / 4;
+                    A[i][j] = round(num);
+                }
             }
         }
     }
+    double end = omp_get_wtime();
+    
     printMatrix(A);
+    printTime(start, end);
+}
+
+void openMP_SOR(int A[SIZE][SIZE], int t) {
+
+    printf("\n\openMP w/SOR w/ %d threads\n",t);
+    //doing odd-even updates on matrix
+    double start = omp_get_wtime();
+    for (int it = 0; it < ITERATIONS; ++it) {
+            omp_set_num_threads(t);
+            #pragma omp parallel
+            for (int i = 1; i < SIZE - 1; ++i) {
+            #pragma omp for nowait
+                //red nodes
+                for (int j = 1; j < SIZE - 1; j += 2) {
+                    int n1 = A[i - 1][j];
+                    int n2 = A[i + 1][j];
+                    int n3 = A[i][j - 1];
+                    int n4 = A[i][j + 1];
+                    double num = (n1 + n2 + n3 + n4) / 4;
+                    A[i][j] = round(num);
+                }
+                #pragma omp for nowait
+                //black nodes
+                for (int j = 2; j < SIZE - 1; j += 2) {
+                    int n1 = A[i - 1][j];
+                    int n2 = A[i + 1][j];
+                    int n3 = A[i][j - 1];
+                    int n4 = A[i][j + 1];
+                    double num = (n1 + n2 + n3 + n4) / 4;
+                    A[i][j] = round(num);
+                }
+                
+            }
+#pragma omp barrier
+
+        }
+    double end = omp_get_wtime();
+
+    printMatrix(A);
+    printTime(start, end);
 }
 
 
 int main() {
     
     int A[SIZE][SIZE]; //original matrix
+    int B[SIZE][SIZE]; //original matrix
+    int t = omp_get_max_threads();
+
     //initializing matrix
    
     for(int i=0;i<SIZE;++i){
@@ -50,15 +106,14 @@ int main() {
             }
         }
     }
-
+    printf("MAX_NUM_OF_THREADS: %d\n", t); //in my case is 4
+    printf("Number of iteration: %d\n", ITERATIONS);
     printf("\n\n PRINT START\n");
-    for (int i = 0; i < SIZE; ++i) {
-        for (int j = 0; j < SIZE; ++j) {
-            printf("%3d ", A[i][j]);
-        }
-        printf("\n");
-    }
-    sequential(A);
-    
+    printMatrix(A);
+    //sequentialJacobi(A);
+    /*for (int i = 2; i <= t; i += 2) {
+        openMP_SOR(A, i);
+    }*/
+    openMP_SOR(A, 4);
     return 0;
 }
